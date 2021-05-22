8f8c840703e78ead90e8214a624f1d8773abb0ae
HY <41522985@qq.com>
2021-05-22 10:13:35 +0800

    add some new future method

--- aioquant/platform/binance.py
+++ aioquant/platform/binance.py
@@ -13,6 +13,8 @@ import json
 import copy
 import hmac
 import hashlib
+import binance_f
+
 from urllib.parse import urljoin
 
 from aioquant.error import Error
@@ -25,7 +27,6 @@ from aioquant.utils.web import Websocket, AsyncHttpRequests
 from aioquant.order import ORDER_ACTION_SELL, ORDER_ACTION_BUY, ORDER_TYPE_LIMIT, ORDER_TYPE_MARKET
 from aioquant.order import ORDER_STATUS_SUBMITTED, ORDER_STATUS_PARTIAL_FILLED, ORDER_STATUS_FILLED, \
     ORDER_STATUS_CANCELED, ORDER_STATUS_FAILED
-
 
 __all__ = ("BinanceRestAPI", "BinanceTrade", )
 
@@ -271,17 +272,74 @@ class BinanceRestAPI:
         success, error = await self.request("DELETE", uri, params=params)
         return success, error
 
-    async def get_klines(self, symbol, startTime, endTime,interval="1m",  limit=500):
+    async def get_klines(self, symbol, start_time=None, end_time=None, interval="1m",  limit=500):
         """Get kilnes list"""
         uri = "/api/v3/klines"
+
         params = {
             "symbol": symbol,
             "interval": interval,
-            "startTime": startTime,
-            "endTime": endTime,
             "limit": limit
         }
+        if(start_time is not None):
+            params.startTime = start_time
+        if(end_time is not None):
+            params.endTime = end_time
         success, error = await self.request("GET", uri, params=params)
+        return success, error
+
+    async def get_futures_transfer(self, start_time, end_time=None, current=1, size=10):
+        """
+        获取合约资金划转历史
+        {
+            "rows": [
+                {
+                "asset": "USDT",          // 资产
+                "tranId": 100000001,      // 划转ID         
+                "amount": "40.84624400",  // 数量
+                "type": "1",              // 划转方向: 1( 现货向USDT本位合约), 2( USDT本位合约向现货), 3( 现货向币本位合约), and 4( 币本位合约向现货)
+                "timestamp": 1555056425000,   // 时间戳
+                "status": "CONFIRMED"         // PENDING (等待执行), CONFIRMED (成功划转), FAILED (执行失败);
+                }
+            ],
+            "total": 1
+        }
+
+        """
+        uri = "/sapi/v1/futures/transfer"
+        params = {
+            "asset": "USDT",
+            "startTime": start_time,
+            "current": current,
+            "size": size,
+            "timestamp": tools.get_cur_timestamp_ms()
+        }
+        if(end_time is not None):
+            params["endTime"] = end_time
+        success, error = await self.request("GET", uri, params=params, auth=True)
+        return success, error
+
+    async def futures_transfer(self, amount, t):
+        """
+        执行现货账户与合约账户之间的划转
+        amount	The amount to be transferred
+        type	1: 现货账户向USDT合约账户划转
+                2: USDT合约账户向现货账户划转
+                3: 现货账户向币本位合约账户划转
+                4: 币本位合约账户向现货账户划转
+
+        {
+            "tranId": 100000001    // 划转 ID 
+        }
+        """
+        uri = "/sapi/v1/futures/transfer"
+        params = {
+            "asset": "USDT",
+            "amount": amount,
+            "type": t,
+            "timestamp": tools.get_cur_timestamp_ms()
+        }
+        success, error = await self.request("POST", uri, params=params, auth=True)
         return success, error
 
     async def request(self, method, uri, params=None, body=None, headers=None, auth=False):
@@ -308,7 +366,162 @@ class BinanceRestAPI:
 
         if data:
             query = "&".join(["=".join([str(k), str(v)])
-                             for k, v in data.items()])
+                              for k, v in data.items()])
+        else:
+            query = ""
+        if auth and query:
+            signature = hmac.new(self._secret_key.encode(
+            ), query.encode(), hashlib.sha256).hexdigest()
+            query += "&signature={s}".format(s=signature)
+        if query:
+            url += ("?" + query)
+
+        if not headers:
+            headers = {}
+        headers["X-MBX-APIKEY"] = self._access_key
+        _, success, error = await AsyncHttpRequests.fetch(method, url, headers=headers, timeout=10, verify_ssl=False)
+        return success, error
+
+
+class BinanceUFutureAPI:
+    """
+    币安交易所API，U本位交易模型
+    """
+
+    def __init__(self, access_key, secret_key, host="https://fapi.binance.com"):
+        """Initialize REST API client."""
+        self._host = host
+        self._access_key = access_key
+        self._secret_key = secret_key
+
+    async def get_position_side(self):
+        """
+        查询用户目前在 所有symbol 合约上的持仓模式：双向持仓或单向持仓。
+        {
+            "dualSidePosition": true // "true": 双向持仓模式；"false": 单向持仓模式
+        }
+        """
+        uri = "/fapi/v1/positionSide/dual"
+        params = {
+            "timestamp": tools.get_cur_timestamp_ms()
+        }
+        success, error = await self.request("GET", uri, params=params, auth=True)
+        return success, error
+
+    async def get_order(self, symbol, order_id=None):
+        """
+        查询订单状态
+
+        请注意，如果订单满足如下条件，不会被查询到：
+            订单的最终状态为 CANCELED 或者 EXPIRED, 并且
+            订单没有任何的成交记录, 并且
+            订单生成时间 + 7天 < 当前时间
+        """
+        uri = "/fapi/v1/order"
+        params = {
+            "symbol": symbol,
+            "timestamp": tools.get_cur_timestamp_ms()
+        }
+        if(order_id is not None):
+            params["orderId"] = order_id
+        success, error = await self.request("GET", uri, params=params, auth=True)
+        return success, error
+
+    async def get_all_orders(self, symbol, order_id=None, start_time=None, end_time=None, limit=500):
+        """Get all account orders; active, canceled, or filled.
+        These orders will not be found:
+        order status is CANCELED or EXPIRED, AND
+        order has NO filled trade, AND
+        created time + 7 days < current time 
+
+        If orderId is set, it will get orders >= that orderId. Otherwise most recent orders are returned.
+        The query time period must be less then 7 days( default as the recent 7 days).
+         """
+        uri = "/fapi/v1/allOrders"
+
+        params = {
+            "symbol": symbol,
+            "timestamp": tools.get_cur_timestamp_ms()
+        }
+        if(order_id is not None):
+            params.orderId = order_id
+        if(start_time is not None):
+            params["startTime"] = start_time
+        if(end_time is not None):
+            params["endTime"] = end_time
+
+        success, error = await self.request("GET", uri, params=params, auth=True)
+        return success, error
+
+    async def get_open_order(self,symbol,order_id=None):
+        """
+        查询当前挂单
+        {
+            "avgPrice": "0.00000",              // 平均成交价
+            "clientOrderId": "abc",             // 用户自定义的订单号
+            "cumQuote": "0",                        // 成交金额
+            "executedQty": "0",                 // 成交量
+            "orderId": 1917641,                 // 系统订单号
+            "origQty": "0.40",                  // 原始委托数量
+            "origType": "TRAILING_STOP_MARKET", // 触发前订单类型
+            "price": "0",                   // 委托价格
+            "reduceOnly": false,                // 是否仅减仓
+            "side": "BUY",                      // 买卖方向
+            "status": "NEW",                    // 订单状态
+            "positionSide": "SHORT", // 持仓方向
+            "stopPrice": "9300",                    // 触发价，对`TRAILING_STOP_MARKET`无效
+            "closePosition": false,   // 是否条件全平仓
+            "symbol": "BTCUSDT",                // 交易对
+            "time": 1579276756075,              // 订单时间
+            "timeInForce": "GTC",               // 有效方法
+            "type": "TRAILING_STOP_MARKET",     // 订单类型
+            "activatePrice": "9020", // 跟踪止损激活价格, 仅`TRAILING_STOP_MARKET` 订单返回此字段
+            "priceRate": "0.3", // 跟踪止损回调比例, 仅`TRAILING_STOP_MARKET` 订单返回此字段
+            "updateTime": 1579276756075,        // 更新时间
+            "workingType": "CONTRACT_PRICE", // 条件价格触发类型
+            "priceProtect": false            // 是否开启条件单触发保护
+        }
+        """
+        uri = "/fapi/v1/openOrder"
+
+        params = {
+            "symbol": symbol,
+            "timestamp": tools.get_cur_timestamp_ms()
+        }
+
+        if(order_id is not None):
+            params["orderId"] = order_id
+        
+        success, error = await self.request("GET", uri, params=params, auth=True)
+        return success, error
+
+
+
+    async def request(self, method, uri, params=None, body=None, headers=None, auth=False):
+        """Do HTTP request.
+
+        Args:
+            method: HTTP request method. `GET` / `POST` / `DELETE` / `PUT`.
+            uri: HTTP request uri.
+            params: HTTP query params.
+            body:   HTTP request body.
+            headers: HTTP request headers.
+            auth: If this request requires authentication.
+
+        Returns:
+            success: Success results, otherwise it's None.
+            error: Error information, otherwise it's None.
+        """
+        url = urljoin(self._host, uri)
+        data = {}
+        if params:
+            data.update(params)
+        if body:
+            data.update(body)
+
+        if data:
+            query = "&".join(["=".join([str(k), str(v)])
+                              for k, v in data.items()])
         else:
             query = ""
         if auth and query:
@@ -569,8 +782,6 @@ class BinanceTrade:
                 order_id = str(order_info["orderId"])
                 order_ids.append(order_id)
             return order_ids, None
-
-
 
     @async_method_locker("BinanceTrade.process.locker")
     async def process(self, msg):
